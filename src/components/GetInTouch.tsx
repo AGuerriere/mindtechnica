82030b4c95860d5ada6044d8bf36c21b13a04c82
Antonio Guerriere <antonioguerriere@gmail.com>
2023-10-15 23:21:27 +0100

    wip get in touch

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -7,8 +7,9 @@ const bayon = Bayon({
 
 export default function GetInTouch() {
   return (
-    <>
-      <h2 className={`${bayon.className} pb-0 text-white text-4xl md:text-6xl lg:text-8xl min-w-fit pb-8 mt-3`}>Get in touch</h2>
+    <div className="flex md:flex-row flex-col">
+     <div>
+     <h2 className={`${bayon.className} pb-0 text-white text-4xl md:text-6xl lg:text-8xl min-w-fit pb-8 mt-3`}>Get in touch</h2>
       <p className="pb-3 text-xs md:text-base">Contact us today to discuss your project requirements and let us bring your vision to life!</p>
       <h5 className="text-white font-semibold pb-3">Email</h5>
       <p className="pb-3 text-xs md:text-base">projects@mindtechnica.com </p>
@@ -23,7 +24,28 @@ export default function GetInTouch() {
           className="inline-block pl-1"
         />
       </h5>
+     </div>
+      <form name="contact-form" method="POST" data-netlify="true" data-netlify-recaptcha="true" className="md:pl-10 md:w-2/5">
+        <div className="flex justify-between pb-3">
+          <label className="block" htmlFor="name">Name</label>
+          <input name="name" id="name" type="text" className="text-greyLight02 rounded-md bg-blueFaded"/>
+        </div>
+        <div className="flex justify-between pb-3">
+          <label htmlFor="company">Company name</label>
+          <input name="company" id="company" type="text" className="text-greyLight02 rounded-md bg-blueFaded" />
+        </div>
+        <div className="flex justify-between pb-3">
+          <label htmlFor="email">Your email</label>
+          <input name="email" id="email" type="email" className="text-greyLight02 rounded-md bg-blueFaded" />
+        </div>
+        <div className="flex justify-between pb-3">
+          <label htmlFor="text">Your Message</label>
+          <textarea name="text" id="text" className="text-greyLight02 rounded-md bg-blueFaded" />
+        </div>
+        <div className="captcha" data-netlify-recaptcha="true"></div>
+        <button type="submit" className="border-solid border-green border-2 rounded-full h-10 w-20">Submit</button>
+      </form>            
 
-    </>
+    </div>
   )
 }
