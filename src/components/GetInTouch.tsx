3d90ea6f10678fbb90a275240841ff71e99f2387
Antonio Guerriere <antonioguerriere@gmail.com>
2023-10-16 14:57:34 +0100

    wip

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -25,26 +25,31 @@ export default function GetInTouch() {
         />
       </h5>
      </div>
-      <form name="mind-technica-form" method="POST" data-netlify="true" data-netlify-recaptcha="true" className="md:pl-10 md:w-2/5">
-        <div className="flex justify-between pb-3">
-          <label className="block" htmlFor="name">Name</label>
-          <input name="name" id="name" type="text" className="text-greyLight02 rounded-md bg-blueFaded" placeholder="Name"/>
-        </div>
-        <div className="flex justify-between pb-3">
-          <label htmlFor="company">Company name</label>
-          <input name="company" id="company" type="text" className="text-greyLight02 rounded-md bg-blueFaded" placeholder="Company Name"/>
-        </div>
-        <div className="flex justify-between pb-3">
-          <label htmlFor="email">Your email</label>
-          <input name="email" id="email" type="email" className="text-greyLight02 rounded-md bg-blueFaded" placeholder="Email"/>
-        </div>
-        <div className="flex justify-between pb-3">
-          <label htmlFor="text">Your Message</label>
-          <textarea name="text" id="text" className="text-greyLight02 rounded-md bg-blueFaded" placeholder="Your Message"/>
-        </div>
-        <div className="captcha" data-netlify-recaptcha="true"></div>
-        <button type="submit" className="border-solid border-green border-2 rounded-full h-10 w-20">Submit</button>
-      </form>            
+        <form name="contact" method="POST" action="/success" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+          <input type="text" name="firstname" id="firstname" />
+            <label htmlFor="yourname">
+              Your Name:
+            </label> <br />
+            <input type="text" name="name" id="yourname" />
+          </p>
+          <p>
+            <label htmlFor="youremail">
+              Your Email:
+            </label> <br />
+            <input type="email" name="email" id="youremail" />
+          </p>
+          <p>
+            <label htmlFor="yourmessage">
+              Message:
+            </label> <br />
+            <textarea name="message" id="yourmessage"></textarea>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
     </div>
   )
 }
