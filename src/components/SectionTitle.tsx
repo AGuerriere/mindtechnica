45d5a35ef0483900319765c558f039fc348b9ec0
Antonio Guerriere <antonioguerriere@gmail.com>
2023-10-17 23:18:41 +0100

    section title line to reach margins

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -12,7 +12,7 @@ export default function SectionTitle(prop:propType) {
   return(
     <div className="flex flex-row align-center mt-20 mb-7">
       <h2 className={`${bayon.className} pb-0 text-white text-4xl md:text-6xl lg:text-8xl min-w-fit`}>{prop.title}</h2>
-      <div className="border-b-2 border-white w-full md:w-2/3 lg:w-3/5"></div>
+      <div className="border-b-2 border-white w-full"></div>
     </div>
   )
 }
