b22c0ecab747ca19c613e75fd54cc3c8e90fcb5b
Antonio Guerriere <antonioguerriere@gmail.com>
2023-10-17 22:43:21 +0100

    added favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import OurWork from '@/components/OurWork'
 import OurClients from '@/components/OurClients'
 import GetInTouch from '@/components/GetInTouch'
 import Footer from '@/components/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 const bayon = Bayon({
@@ -20,6 +21,15 @@ const bayon = Bayon({
 export default function Home(){
   return (
     <>  
+    <Head>
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+    <link
+      rel="apple-touch-icon"
+      href="/apple-icon.png"
+      type="image/<generated>"
+      sizes="<generated>"
+      />
+    </Head>
       <Header />
       <main>
         <AboutUs />
