b28ac3fa7f8d3efd5b29b33414ff7bf895607eed
Antonio Guerriere <antonioguerriere@gmail.com>
2023-10-19 22:43:05 +0100

    fix for favicon not showing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,10 @@ export default function Home(){
   return (
     <>  
     <Head>
-    <link rel="icon" href="/favicon.ico" sizes="any" />
-    <link
-      rel="apple-touch-icon"
-      href="/apple-icon.png"
-      type="image/<generated>"
-      sizes="<generated>"
-      />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       <link
         rel="canonical"
         href="https://mindtechnica.com"
