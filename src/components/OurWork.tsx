e8668b66f93ee1564acf48e9d863f74bafc4774b
Antonio Guerriere <antonioguerriere@gmail.com>
2023-12-16 13:48:22 +0000

    added client pics

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -23,13 +23,13 @@ export default function OurWork(){
         />
       <OurWorkItem 
         image="/images/brand2.webp"
-        brand="Brand 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        brand="Watsons Opticians"
+        description="We are proud to have created a brand new identity for our client, which is reflected in the stunning design of their website. Our team worked tirelessly to ensure that the website is not only visually appealing but also user-friendly and easy to navigate"
         />
       <OurWorkItem 
         image="/images/brand3.webp"
-        brand="Brand 3"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        brand="McHenry Bros Ltd"
+        description="We helped our client to redefine their brand identity by focusing on their core values, which is reflected in the updated branding of their website. Additionally, we created a faster and more responsive website that has been generating more leads for our client."
         />
         
     </div>
--- src/components/OurWorkItem.tsx
+++ src/components/OurWorkItem.tsx
@@ -9,14 +9,14 @@ type propsType = {
 export default function OurWorkItem(props:propsType) {
   return(
     <div className="flex flex-col items-start w-80">
-     <Image
-        src={props.image}
-        width={520}
-        height={380}
-        alt="Brand 1"
-        // The filler images are already rounded so you might need to fix border raidus to keep same round corners.
-        className="pb-5 rounded-md md:rounded-3xl"
-      />
+        <Image
+            src={props.image}
+            width={500}
+            height={500}
+            alt="Brand 1"
+            // The filler images are already rounded so you might need to fix border raidus to keep same round corners.
+            className="pb-5 rounded-md md:rounded-3xl"
+          />
       <h5 className="text-sm pb-5 md:text-xl text-white font-semibold">{props.brand}</h5>
       <p className="text-xs md:text-sm md:w-2/3">{props.description}</p>
     </div>
