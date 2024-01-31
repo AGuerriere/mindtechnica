ad9ffbaf322c62d2149f69aa59cd691b2c6ee5b3
Antonio Guerriere <antonioguerriere@gmail.com>
2024-01-31 15:57:10 +0000

    on mobile screens our work shows as a column

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -15,7 +15,7 @@ export default function OurWork(){
         />
       </div>
     <SectionTitle title="Our Work"/>
-    <div className="flex justify-between gap-3">
+    <div className="flex flex-col md:flex-row justify-between gap-3">
       <OurWorkItem 
         image="/images/brand1.webp"
         brand="GetSociable"
--- src/components/OurWorkItem.tsx
+++ src/components/OurWorkItem.tsx
@@ -8,17 +8,17 @@ type propsType = {
 
 export default function OurWorkItem(props: propsType) {
   return (
-    <div className="flex flex-col items-start w-1/4">
+    <div className="flex flex-col items-start md:w-1/4 pb-3">
       <div className="h-auto w-full aspect-[4/3] relative p-5">
         <Image
           src={props.image}
           fill
           alt="Brand 1"
           // The filler images are already rounded so you might need to fix border raidus to keep same round corners.
-          className="rounded-lg object-fill"
+          className="rounded-3xl object-fill"
         />
       </div>
-        <h5 className="text-sm pb-5 md:text-xl text-white font-semibold">{props.brand}</h5>
+        <h5 className="text-sm pb-5 pt-5 md:text-xl text-white font-semibold">{props.brand}</h5>
         <p className="text-xs md:text-sm md:w-2/3">{props.description}</p>
     </div>
   )
