115d3b708589d57358e8abcf37c056ba0be6527f
Antonio Guerriere <antonioguerriere@gmail.com>
2024-02-29 09:59:41 +0000

    changed the style of the form button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -86,32 +86,33 @@ export default function Contact() {
             placeholder="Type your message here*"
             id="yourmessage"></textarea>
         </div>
-
-        <button
-          type="submit"
-          className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
-          {isSubmitting ? (
-            <svg
-              className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24">
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-          ) : (
-            "Send Message"
-          )}
-        </button>
+        <div className="flex justify-start">
+          <button
+            type="submit"
+            className="border-solid border-green border-2 rounded-2xl h-12 w-32">
+            {isSubmitting ? (
+              <svg
+                className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24">
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            ) : (
+              "Send"
+            )}
+          </button>
+        </div>
       </form>
 
       {isSubmitSuccessful && isSuccess && (
