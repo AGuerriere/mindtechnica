2ec53087a2b4834459291f49f1a5d1cf365db45a
Antonio Guerriere <antonioguerriere@gmail.com>
2026-03-20 18:04:19 +0000

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,6 +12,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/careers`,
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
